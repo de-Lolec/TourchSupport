05745cc672d4d1ccaf5f7628a472700bd6df8065
deniskorbakov <korbakovd@gmail.com>
2023-11-18 01:39:58 +0300

    feat: add new paths

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
+use App\Livewire\Company;
+use App\Livewire\Home;
+use App\Livewire\Portfolio;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Home::class);
+Route::get('/portfolio', Portfolio::class);
+Route::get('/company', Company::class);
