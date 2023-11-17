95a0993bb910b783f433fe09310f243a1b7b4b0e
de-lolec <matvey20158@mail.ru>
2023-11-17 19:59:36 +0300

    feat: add migrations categories and contact

--- database/migrations/2023_11_17_154954_create_contact_table.php
+++ database/migrations/2023_11_17_154954_create_contact_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Category;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -11,14 +13,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('contact', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('type');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Category::class);
+            $table->foreignId('staff_id');
+            $table->boolean('is_important');
+            $table->text('text');
+            $table->string('priority');
             $table->timestamps();
         });
     }
@@ -28,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('contact');
     }
 };
