79ac7901b24cef69941fdcf0dec1dc3d256b67f1
de-lolec <matvey20158@mail.ru>
2023-11-18 14:03:30 +0300

    dev: add siders for categories

--- app/Filament/Widgets/OrdersChart.php
+++ app/Filament/Widgets/OrdersChart.php
@@ -23,38 +23,28 @@ class OrdersChart extends ChartWidget
 
         $currentDate = Carbon::now();
 
-        // Создаем массив для хранения количества записей в каждый день недели
         $recordsPerDay = [];
 
         for ($day = 0; $day < 7; $day++) {
-            // Получаем начало и конец текущего дня
+
             $startOfDay = $currentDate->copy()->startOfDay()->subDays($day);
             $endOfDay = $currentDate->copy()->endOfDay()->subDays($day);
         
-            // Получаем количество записей за текущий день
             $recordsCount = Contact::whereBetween('created_at', [$startOfDay, $endOfDay])->count();
         
-            // Добавляем количество записей в массив
             $recordsPerDay[] = $recordsCount;
         }
         $daysOfWeek = [];
 
-        // Получаем текущую дату
         $startDate = Carbon::now();
     
-        // Вычитаем 7 дней
         $startDate->subDays(7);
-    
-        // Заполняем массив с датами за последние 7 дней в формате "день.месяц"
+
         for ($i = 0; $i < 7; $i++) {
             $daysOfWeek[] = $startDate->format('d.m');
             $startDate->addDay();
         }
     
-        // dd($daysOfWeek);
-
-        // dd($recordsPerDay);
-
         return [
             'datasets' => [
                 [
