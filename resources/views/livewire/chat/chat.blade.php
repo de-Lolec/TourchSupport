4342bd6c1dbf176ca5e571555c7d9d477892745f
deniskorbakov <korbakovd@gmail.com>
2023-11-18 18:28:32 +0300

    feat: add example page

---
+++ resources/views/livewire/chat/chat.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <div class="container mx-auto text-center">
+        <div class="flex flex-row max-[1150px]:flex-col">
+            <div class="w-1/2 p-2 mt-10 max-[1150px]:mx-auto max-lg:w-full">
+                project
+            </div>
+
+            <div class="w-1/2 p-2 mt-10 max-[1150px]:hidden">
+                project
+            </div>
+        </div>
+    </div>
+</div>
