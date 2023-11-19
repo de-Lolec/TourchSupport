d47446dbda1bc30550397a088c72b73019afe925
de-lolec <matvey20158@mail.ru>
2023-11-19 15:36:38 +0300

    dev: delete comments

--- app/Livewire/FormProblem.php
+++ app/Livewire/FormProblem.php
@@ -6,7 +6,6 @@ use App\Models\Contact;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 use App\Facades\MachineLearningManager;
-// use App\Services\MachineLearningManager;
 
 class FormProblem extends Component
 {
@@ -20,14 +19,14 @@ class FormProblem extends Component
     public $checkbox = 0;
 
     public function save() {
-        // $this->validate([
-        //     'file' => 'nullable|image|max:10000',
-        //     'fio' => 'required',
-        //     'email' => 'required|email',
-        //     'phone' => 'required|min:11|max:12',
-        //     'text' => 'required|max:255|min:10',
-        //     'checkbox' => 'accepted'
-        // ]);
+        $this->validate([
+            'file' => 'nullable|image|max:10000',
+            'fio' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required|min:11|max:12',
+            'text' => 'required|max:255|min:10',
+            'checkbox' => 'accepted'
+        ]);
 
         $contact = Contact::create([
             'path_file' => $this->file ? $this->file->store('photos') : 'no-photo-available.png',
@@ -36,7 +35,7 @@ class FormProblem extends Component
             'phone' => $this->phone,
             'text' => $this->text,
         ]);
-        // dd($contact);
+
         $fields = MachineLearningManager::getImportancyAndPriority($this->text, $contact);
 
         return redirect()->to('/chat');
