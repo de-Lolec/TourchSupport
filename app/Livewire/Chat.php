1d698cdc2fa21aef44a6280fde7bb94fcc8f0db6
deniskorbakov <korbakovd@gmail.com>
2023-11-19 15:08:38 +0300

    feat: add data is database

--- app/Livewire/Chat.php
+++ app/Livewire/Chat.php
@@ -2,12 +2,15 @@
 
 namespace App\Livewire;
 
+use App\Models\Contact;
 use Livewire\Component;
 
 class Chat extends Component
 {
     public function render()
     {
-        return view('livewire.chat.chat');
+        return view('livewire.chat.chat', [
+            'ticket' => Contact::all()->last()
+        ]);
     }
 }
