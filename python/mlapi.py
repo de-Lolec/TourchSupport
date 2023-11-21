a224e02a1ab4e97e9839e60233ec30c7c16aca05
de-lolec <m_egrishin@webjox.ru>
2023-11-21 20:33:22 +0300

    feat: added flask in docker

--- python/mlapi.py
+++ python/mlapi.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, jsonify, request
 import numpy as np
 import pandas as pd
@@ -10,18 +9,30 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
 import joblib
-
+import os
 
 app = Flask(__name__)
 
 
+def load_model(file_name):
+    script_dir = os.path.dirname(__file__)
+    model_path = os.path.join(script_dir, file_name)
+
+    try:
+        model = joblib.load(model_path)
+    except Exception as e:
+        print(f"Error loading model: {e}")
+        model = None
+
+    return model
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Загрузка сохраненных моделей и кодировщика
-    nb_model_request = joblib.load('nb_model_request.pkl')
-    le_request = joblib.load('label_encoder_request.pkl')
-    vectorizer = joblib.load('vectorizer.pkl')
-    rf_model_importance = joblib.load('rf_model_importance.pkl')
+    script_dir = os.path.dirname(__file__)
+    nb_model_request = load_model('nb_model_request.pkl')
+    le_request = load_model('label_encoder_request.pkl')
+    vectorizer = load_model('vectorizer.pkl')
+    rf_model_importance = load_model('rf_model_importance.pkl')
     # Пример нового обращения
     # new_utterance = "I have a question about my order delivery."
     data = request.get_json()
@@ -38,4 +49,4 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
